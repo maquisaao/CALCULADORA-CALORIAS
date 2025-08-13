8ab8c2156d58168cbb748e0f58f13d55036879c3
maquisaao <maxwpinheiro@gmail.com>
2025-08-13 12:48:32 -0300

    menu e entrada de dados(etapa1)

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-# receber os dados do usuario: Nome, Idade, Peso, Altura, Gênero e Frequencia de atividade fisica
+# fazer menu de entrada e receber os dados do usuario: Nome, Idade, Peso, Altura, Gênero e Frequencia de atividade fisica
+escolha = input('[E]ntrar [S]air \n')
+while 'E' in escolha:
+
+    nome = input('Qual seu nome? ')
+    idade = input('Qual sua idade? ')
+    peso = input('Qual seu peso? ')
+    altura = input('Qual sua altura? ')
+    genero = input('Qual seu gênero, [H]omem ou [M]ulher? ')
+    frequencia_fisica = input(
+        'Sua frequencia de atividade fisica é [L]eve, [M]oderada ou [I]ntensa? ')
 
 # pela atividade fisica e genero, multiplicar pelo valor
 #printar o resultado junto com o nome do usuario

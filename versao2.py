daafad2a9e4b47dfb3034860c8dd89cff0f24e67
maquisaao <maxwpinheiro@gmail.com>
2025-08-31 20:58:57 -0300

    feat: adicionar funcao pra calcular o gasto calorico

--- versao2.py
+++ versao2.py
@@ -3,3 +3,19 @@
 # garantir que letras minusculas e maiusculas sejam aceitas
 # simular tempo de espera pro calculo ( enfeite )
 
+# fucao pro gasto 
+def calcular_gasto_calorico(peso, genero, frequencia):
+    # Constantes diferentes para homens e mulheres
+    if genero == 'H':
+        base = (0.063 * peso + 2.896) * 239
+    else:  # genero == 'M'
+        base = (0.062 * peso + 2.036) * 239
+
+    # Fatores de multiplicação pela frequência
+    fatores = {
+        'L': 1.11,
+        'M': 1.25,
+        'I': 1.48
+    }
+    return base * fatores[frequencia]
+
